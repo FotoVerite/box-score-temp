1c7de63b915171543518709e2e01c46c8febc5b3
Taylor Kearns <tkearns@gmail.com>
2012-09-25 16:59:03 -0400

    CanCan initial implementation

---
+++ app/models/ability.rb
@@ -0,0 +1,11 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(admin)
+    admin ||= Admin.new
+
+    if admin.present?
+      #can :manage, School, id: admin.school_id
+    end
+  end
+end
