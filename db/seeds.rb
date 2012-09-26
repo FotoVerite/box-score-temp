0e520080f034d8eb4176e5a00f80c98868a57982
Taylor Kearns <tkearns@gmail.com>
2012-09-26 15:11:09 -0400

    School + Admin seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def create_admin(school, name)
+  first_name = name
+  last_name = 'Admin'
+  password = 'testing'
+  school_id = school.id
+  admins = Admin.create! email: "#{first_name}.#{last_name}@example.com",
+                         first_name: first_name, last_name: last_name, password: password,
+                         school_id: school_id
+end
+
+def create_school(str)
+  name = "#{str} High School"
+  city = ['Brooklyn', 'Manhattan', 'Queens', 'Staten Island', 'Bronx'].sample
+  state = 'New York'
+  organization_type = ['PSAL', 'CHSAA', 'NYSAISAA'].sample
+  schools = School.create! name: name, city: city, state: state,
+                           organization_type: organization_type
+end
+
+Admin.delete_all
+School.delete_all
+
+names = ['Trinity', 'Southville', 'Lexington', 'Eastern Catholic', 'Westminster']
+
+(1..5).each do |n|
+  name = names[n-1]
+
+  school = create_school name
+  create_admin school, name
+end
