f60b51243e86a87d946a060aae199169621107de
Taylor Kearns <tkearns@gmail.com>
2012-09-30 17:46:18 -0400

    Refactor admin auth
    Render text and unauthorized status instead of redirect to :back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
-    respond_with :unauthorized
+    render text: 'Unauthorized', status: :unauthorized
   end
 
   def after_sign_in_path_for(admin)
