a0aed9444b27885469eacc54d3510210f871ca65
Taylor Kearns <tkearns@gmail.com>
2012-10-01 17:23:16 -0400

    Simple layout for school show/edit views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
       class: ['box-score', controller.controller_name, controller.action_name].join(' ')
     }
   end
+
+  def league_assns
+    League.uniq.map{ |league| ["#{league.name} (#{league.assn.name})", league.id] }
+  end
 end
