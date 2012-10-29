0fd32586e6795736e7cffcd6a9c75cdaafa42b91
Jeremy Weiskotten <jeremy@terriblelabs.com>
2012-10-29 11:33:06 -0400

    fix seed script, add games

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+Admin.delete_all
+School.delete_all
+Assn.delete_all
+League.delete_all
+Season.delete_all
+Player.delete_all
+Team.delete_all
+Game.delete_all
+
+
 SCHOOL_NAMES = ['Trinity', 'Southville', 'Lexington', 'Eastern Catholic', 'Westminster',
                'Northern', 'Lakeland', 'Bishop Kelly', 'Rexburg', 'Southern', 'Eastminster', 'Western Catholic']
 ASSOCIATION_NAMES = ['PSAL', 'CHSAA', 'NYSAISAA']
@@ -24,15 +34,6 @@ end
 def create_league(name, assn_id)
   league = League.create! name: name, assn_id: assn_id
 end
-
-Admin.delete_all
-School.delete_all
-Assn.delete_all
-League.delete_all
-Season.delete_all
-Player.delete_all
-Team.delete_all
-Game.delete_all
 
 (1..3).each do |n|
   name = ASSOCIATION_NAMES[n-1]
@@ -90,28 +91,57 @@ end
   create_admin school, name
 end
 
-Season.create!(start_date: Date.new(2012, 9, 9), year: '2012-2013', current: true)
-Season.create!(start_date: Date.new(2012, 9, 9), year: '2012-2013', current: true)
-Season.create!(start_date: Date.new(2012, 9, 9), year: '2012-2013', current: true)
-Season.create!(start_date: Date.new(2012, 9, 9), year: '2012-2013', current: true)
+boys_baseball_season    = Season.create!(start_date: Date.new(2012, 9, 9), year: '2012-2013', current: true)
+boys_basketball_season  = Season.create!(start_date: Date.new(2012, 9, 9), year: '2012-2013', current: true)
+girls_softball_season   = Season.create!(start_date: Date.new(2012, 9, 9), year: '2012-2013', current: true)
+girls_basketball_season = Season.create!(start_date: Date.new(2012, 9, 9), year: '2012-2013', current: true)
+
+# prior seasons
 Season.create!(start_date: Date.new(2011, 9, 9), year: '2011-2012')
 Season.create!(start_date: Date.new(2011, 9, 9), year: '2011-2012')
 Season.create!(start_date: Date.new(2011, 9, 9), year: '2011-2012')
 Season.create!(start_date: Date.new(2011, 9, 9), year: '2011-2012')
 
-(0..11).each do |num|
-  Team.create!(sport: 'Boys Baseball', school_id: School.all.to_a[num].id, season_id: Season.all.to_a[0].id)
-  Team.create!(sport: 'Boys Basketball', school_id: School.all.to_a[num].id, season_id: Season.all.to_a[1].id)
-  Team.create!(sport: 'Girls Softball', school_id: School.all.to_a[num].id, season_id: Season.all.to_a[2].id)
-  Team.create!(sport: 'Girls Basketball', school_id: School.all.to_a[num].id, season_id: Season.all.to_a[3].id)
+School.all.each do |school|
+  Team.create!(sport_id: 'boys-baseball', league_id: League.all.sample.id, school_id: school.id, season_id: boys_baseball_season.id)
+  Team.create!(sport_id: 'boys-basketball', league_id: League.all.sample.id, school_id: school.id, season_id: boys_basketball_season.id)
+  Team.create!(sport_id: 'girls-softball', league_id: League.all.sample.id, school_id: school.id, season_id: girls_softball_season.id)
+  Team.create!(sport_id: 'girls-basketball', league_id: League.all.sample.id, school_id: school.id, season_id: girls_basketball_season.id)
+end
+
+def random_game_stats(team, opponent)
+  case team.sport_type
+  when 'basketball'
+    {
+      team.id.to_s => {
+        "periods" => Hash[(1..4).map { |p| [p.to_s, rand(25).to_s] }],
+      },
+      opponent.id.to_s => {
+        "periods" => Hash[(1..4).map { |p| [p.to_s, rand(25).to_s] }],
+      }
+    }
+  when 'baseball'
+    {
+      team.id.to_s => {
+        "periods" => Hash[(1..7).map { |p| [p.to_s, rand(4).to_s] }],
+        "hits" => rand(20).to_s,
+        "errors" => rand(5).to_s
+      },
+      opponent.id.to_s => {
+        "periods" => Hash[(1..7).map { |p| [p.to_s, rand(4).to_s] }],
+        "hits" => rand(20).to_s,
+        "errors" => rand(5).to_s
+      }
+    }
+  end
 end
 
 Team.all.each do |team|
-  other_teams = Team.where('sport = :sport and id != :id', sport: team.sport, id: team.id)
-  other_teams.each do |opponent|
-    random_date = Time.at(1319471760.092567 + rand * (Time.now.to_f - 1319471760.092567.to_f)).stamp('2011-11-11')
-    game = Game.create!(date: random_date, home_away: 'home', site: 'home', team_id: team.id, opponent_id: opponent.id)
-    game.save!
+  team.potential_opponents.each do |opponent|
+    random_date = team.season.start_date + rand(100)
+
+    Game.create!(date: random_date, home_away: 'home', site: 'home',
+      team_id: team.id, opponent_id: opponent.id, game_stats: random_game_stats(team, opponent))
   end
 end
 
