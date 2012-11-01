c52603668e61851067db7f9cbd5528a1d33e8d6d
Taylor Kearns <tkearns@gmail.com>
2012-11-01 13:09:59 -0400

    Share box score via email

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -8,4 +8,8 @@ module GamesHelper
   def twitter_box_score_message(team, opponent, date, url)
     "Box score for #{team.display_name} vs. #{opponent.display_name}, #{date.stamp('11/11/99')} is now posted: #{url}"
   end
+
+  def box_score_email_link(game)
+    CGI::escape "Box score posted: #{game.team.display_name} vs. #{game.opponent.display_name} #{game.date}: #{game_url(game)}"
+  end
 end
