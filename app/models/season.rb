2679f8ac4430940473f0086f4244e98ba2a3d33d
Taylor Kearns <tkearns@gmail.com>
2012-11-05 15:04:41 -0500

    Removed attr_accessible :end_date from Season

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,5 +1,5 @@
 class Season < ActiveRecord::Base
-  attr_accessible :end_date, :year, :start_date, :current, :sport_id
+  attr_accessible :year, :start_date, :current, :sport_id
 
   validates_presence_of :year, :start_date
 
