df94830f4aaa1d465f55c72292e396c493a8d2b9
Jeremy Weiskotten <jeremy@terriblelabs.com>
2012-11-12 11:10:56 -0500

    redirect to www subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :ensure_subdomain
+
   rescue_from CanCan::AccessDenied do |exception|
     render text: 'Unauthorized', status: :unauthorized
   end
@@ -15,5 +17,9 @@ class ApplicationController < ActionController::Base
 
   def authenticate_admin_user!
     redirect_to '/' unless current_admin.try(:superadmin?)
+  end
+
+  def ensure_subdomain
+    redirect_to subdomain: 'www' unless request.subdomain == 'www'
   end
 end
