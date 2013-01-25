fce979bae43f4ed83162ffe2c020921cdf1d13a6
Taylor Kearns <tkearns@gmail.com>
2013-01-25 11:37:32 -0500

    Removed pending unwritten tests for adding box scores

--- spec/acceptance/add_box_score.feature
+++ spec/acceptance/add_box_score.feature
@@ -4,9 +4,3 @@ Feature: Add a box score
 
   Scenario: Adding a box score for a basketball game
     Given I have a basketball team
-    When I click 'Add Box Score'
-    And I select my basketball team
-
-    Then the potential opponents list should update
-    And the game stats table should update with basketball fields
-    And the player game stats table should update with basketball player fields
--- spec/acceptance/steps/add_box_score_steps.rb
+++ spec/acceptance/steps/add_box_score_steps.rb
@@ -1,25 +1,3 @@
 step "I have a basketball team" do
   @basketball_team = create :team
 end
-
-step "I click 'Add Box Score'" do
-  # this will fail when the admin  menu is hidden
-  pending
-  #click_link 'Add Box Score'
-end
-
-step "I select my basketball team" do
-  pending
-  #select("#{@basketball_team.display_name}", from: 'Team')
-end
-
-step "the potential opponents list should update" do
-end
-
-step "the game stats table should update with basketball fields" do
-end
-
-step "the player game stats table should update with basketball player fields" do
-end
-
-
