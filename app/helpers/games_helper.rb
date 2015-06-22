425e769a33a68b31cec4ee12c85fceb0e3258b33
Elijah Kim <kimelijah90@gmail.com>
2015-06-22 12:05:43 -0400

    Add Facebook and Twitter functionality to posts
    * Bonus - some styles to post body

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -5,11 +5,18 @@ module GamesHelper
     end
   end
 
-  def twitter_box_score_message(team, opponent, date, url)
-    "Box score for #{team.school.name} vs. #{opponent.school.name} #{team.sport.name} #{date.stamp('11/11/99')} #{url}"
+  def twitter_box_score_message(game)
+    CGI::escape <<EOT.squish
+Box score for #{game.team.school_name} vs. #{game.opponent.school_name}
+ #{game.team.sport.name} #{game.date.stamp('11/11/99')} #{game_url(game)}
+EOT
   end
 
   def box_score_email_link(game)
     CGI::escape "Box score posted: #{game.team.display_name} vs. #{game.opponent.display_name} #{game.date}: #{game_url(game)}"
+  end
+
+  def twitter_post_message(post)
+    CGI::escape "#{post.title} #{post_url(post)}"
   end
 end
