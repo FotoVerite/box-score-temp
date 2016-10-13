2c4e119e6bc4e02c7c2c75056a51173a384ae41f
FotoVerite <mzbphoto@gmail.com>
2016-10-13 17:12:39 -0400

    adds in region.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,7 +3,9 @@ if ENV['S3_SECRET_ACCESS_KEY']
   Paperclip::Attachment.default_options[:s3_credentials] = {
       bucket: ENV['S3_BUCKET'],
       access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
+      secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
+      region: ENV['S3_REGION']
+
     }
   Paperclip::Attachment.default_options[:s3_protocol] = :https
   Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
