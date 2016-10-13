28921f807b0089a62590657ddce9d7390a87004e
FotoVerite <mzbphoto@gmail.com>
2016-10-13 17:18:30 -0400

    testing paperclip fix.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,6 @@
 if ENV['S3_SECRET_ACCESS_KEY']
   Paperclip::Attachment.default_options[:storage] = :s3
+  Paperclip::Attachment.default_options[:s3_host_alias] = 's3-us-west-2.amazonaws.com/hsboxscoresnyc-staging'
   Paperclip::Attachment.default_options[:s3_credentials] = {
       bucket: ENV['S3_BUCKET'],
       access_key_id: ENV['S3_ACCESS_KEY_ID'],
