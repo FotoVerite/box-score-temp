e004686cee08a0fcb6eff04b210271b7c3c197f1
FotoVerite <mzbphoto@gmail.com>
2016-10-16 15:43:04 -0400

    trying custom size.

--- db/migrate/20161016185052_add_admin_id_to_game_and_post.rb
+++ db/migrate/20161016185052_add_admin_id_to_game_and_post.rb
@@ -1,6 +1,15 @@
 class AddAdminIdToGameAndPost < ActiveRecord::Migration[5.0]
-  def change
-    add_column :games, :admin_id, :integer
-    add_column :posts, :admin_id, :integer
+  def up
+    if column_exists?(:games, :admin_id)
+      add_column :games, :admin_id, :integer
+      add_column :posts, :admin_id, :integer
+      add_index :games, :admin_id
+      add_index :posts, :admin_id
+    end
+  end
+
+   def down
+    remove_column :games, :admin_id
+    remove_column :posts, :admin_id
   end
 end
