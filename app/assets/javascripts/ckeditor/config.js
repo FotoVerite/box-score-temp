80759edbbf54b38323de70256049a6a18f57dc3a
FotoVerite <mzbphoto@gmail.com>
2016-10-20 19:15:38 -0400

    adds embeding for links.

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -12,7 +12,7 @@ CKEDITOR.editorConfig = function( config )
   config.fontSize_defaultLabel = '18px';
   config.autoGrow_maxHeight = 400;
   config.height = '30em';
-  config.extraPlugins = 'youtube';
+  config.extraPlugins = 'youtube,embed'
   config.allowedContent = true;
 
   config.toolbar = [
@@ -22,7 +22,7 @@ CKEDITOR.editorConfig = function( config )
     { name: 'colors', items: [ 'TextColor' ] },
     { name: 'paragraph', groups: [ 'mode', 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
     { name: 'links', items: [ 'Link', 'Unlink' ] },
-    { name: 'insert', items: ['Image', 'Youtube'] }
+    { name: 'insert', items: ['Image', 'Youtube', 'Embed'] }
 
   ];
 
