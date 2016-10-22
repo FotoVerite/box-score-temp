90798be531873bebe876f82215d31dcd0a01105d
FotoVerite <mzbphoto@gmail.com>
2016-10-22 13:18:23 -0400

    small fixes.

---
+++ db/migrate/20161022170704_all_posts_belong_to_patrick.rb
@@ -0,0 +1,6 @@
+class AllPostsBelongToPatrick < ActiveRecord::Migration[5.0]
+  def up
+    admin_id = Admin.where(email: 'patrick@hsboxscoresnyc.com').first.id
+    Post.update_all(:admin_id => admin_id)
+  end
+end
