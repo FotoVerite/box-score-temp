883427682c987c17b9e72a5a5bd9a82ef2f67d51
FotoVerite <mzbphoto@gmail.com>
2016-11-07 00:18:56 -0500

    set host name in heroku not in app.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,12 +1,10 @@
 if ENV['S3_SECRET_ACCESS_KEY']
   Paperclip::Attachment.default_options[:storage] = :s3
-  Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+  Paperclip::Attachment.default_options[:s3_host_name] = ENV['S3_HOST_NAME']
   Paperclip::Attachment.default_options[:s3_credentials] = {
       bucket: ENV['S3_BUCKET'],
       access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-      region: ENV['S3_REGION']
-
+      secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
     }
   Paperclip::Attachment.default_options[:s3_protocol] = :https
   Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
