692b757c9fa11a56c2832fa344ff97adc167fa71
Matthew Bergman <mzbphoto@gmail.com>
2019-04-25 14:14:01 -0400

    Update game to box score and fix sort order to boys first.

--- app/models/games_grouping.rb
+++ app/models/games_grouping.rb
@@ -4,6 +4,9 @@ class GamesGrouping
   end
 
   def each
+    # TODO this entire filter concept is broken and should be using simple sql and scopes not this
+    # Ordering is nearly impossible, order commands are not working as expected. The entire thing is over complicated what is basic filters
+    @games = @games.sort_by {|g| g.sport.name}
     @games.group_by(&:sport).each do |sport, games|
       yield sport, DateGrouping.new(games)
     end
